65c35d831fe3f8a1bd906e5987e981ab3e6bf7d5
tungsing96 <820841859@qq.com>
2022-06-29 21:10:23 +0800

    submit file

---
+++ src/main/java/com/atguigu/git/Test.java
@@ -0,0 +1,10 @@
+package com.atguigu.git;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("test");
+        System.out.println("test");
+        System.out.println("test");
+        System.out.println("test");
+    }
+}
